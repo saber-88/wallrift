4d929d32c8edc70925471a49e2904a30a2876563
karmveer <karmveervaghela@gmail.com>
2026-04-14 14:05:30 +0530

    refactored code

--- src/daemon.c
+++ src/daemon.c
@@ -12,6 +12,7 @@
 #include <GL/gl.h>
 #include <GL/glext.h>
 #include <GLES2/gl2.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -96,7 +97,7 @@ GLuint loadImageIntoGPU(char *imgPath, int *imageWidth, int* imageHeight, GLuint
   int channels;
   unsigned char *pixels = stbi_load(expanded, imageWidth, imageHeight, &channels, 4);
   if (!pixels) {
-    printf("\n[ERR][STB]: Cannot load image\n");
+    fprintf(stderr,"\n[ERR][STB]: Cannot load image\n");
     return texID;
   }
 
@@ -148,7 +149,7 @@ GLuint createShader(GLenum type, const char *shaderSrc) {
   glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
   if (!success) {
     glGetShaderInfoLog(shader, 512, NULL, infoLog);
-    printf("[ERR][SHADER]: %s\n", infoLog);
+    fprintf(stderr,"[ERR][SHADER]: %s\n", infoLog);
     return -1;
   }
   return shader;
@@ -158,14 +159,18 @@ GLuint createProgram(const char *vFilePath, const char *fFilePath) {
   const char *vsrc = readFile(vFilePath);
   const char *fsrc = readFile(fFilePath);
   if (!vsrc || !fsrc) {
-    printf("[ERR][FILE]: %s\n", "Failed to open shader file.");
+    fprintf(stderr,"[ERR][FILE]: %s\n", "Failed to open shader file.");
+    free((void*)vsrc);
+    free((void*)fsrc);
     return -1;
   }
   GLuint vs = createShader(GL_VERTEX_SHADER, vsrc);
   GLuint fs = createShader(GL_FRAGMENT_SHADER, fsrc);
 
   if (vs == -1 || fs == -1) {
-    printf("[ERR][SHADER]: %s\n", "Failed to create shader.");
+    fprintf(stderr,"[ERR][SHADER]: %s\n", "Failed to create shader.");
+    free((void*)vsrc);
+    free((void*)fsrc);
     return -1;
   }
   GLuint prog = glCreateProgram();
@@ -181,7 +186,7 @@ GLuint createProgram(const char *vFilePath, const char *fFilePath) {
   glGetProgramiv(prog, GL_LINK_STATUS, &success);
   if (!success) {
     glGetProgramInfoLog(prog, 512, NULL, infoLog);
-    printf("[ERR][LINKING] :%s\n", infoLog);
+    fprintf(stderr,"[ERR][LINKING] :%s\n", infoLog);
   }
   glDeleteShader(vs);
   glDeleteShader(fs);
@@ -206,7 +211,7 @@ unsigned int indices[] = { 0, 1, 2, 2, 3, 0 };
 float speed = 0.03f;
 
 // wallpaper width and height
-int img_w , img_h;
+int img_w = 0, img_h = 0;
 
 void gl_draw(WL *wl, GL *gl, int texloc, GLuint textureId){
     
@@ -230,6 +235,190 @@ void gl_draw(WL *wl, GL *gl, int texloc, GLuint textureId){
     eglSwapBuffers(wl->egl_display, wl->egl_surface);
 
 }
+
+void setupWayland(WL *wl){
+  wl->run = 1;
+  wl->cursor_x = 0.05f; // initialize it at centre 
+  wl->display = wl_display_connect(NULL);
+  if (!wl->display) {
+      fprintf(stderr, "[ERR][WAYLAND]: Failed to connect\n");
+      exit(1);
+  }
+  wl->registry = wl_display_get_registry(wl->display);
+  wl_registry_add_listener(wl->registry, &registry_listener, wl);
+  wl_display_roundtrip(wl->display);
+}
+
+void setupCursor(WL *wl){
+
+  if (wl->shm) {
+    /* supports X_CURSOR only cause hyprcursor uses
+     * different format for cursor and does not have API
+     * to work with.
+     */
+    char* theme = getenv("XCURSOR_THEME");
+    char* cursor_size = getenv("XCURSOR_SIZE");
+    
+    int size = cursor_size ? atoi(cursor_size) : 24;
+
+    wl->cursor_theme = wl_cursor_theme_load(theme,size, wl->shm);
+    if (wl->cursor_theme) {
+      wl->cursor = wl_cursor_theme_get_cursor(wl->cursor_theme, "default");
+    }
+    if (!wl->cursor) {
+      wl->cursor = wl_cursor_theme_get_cursor(wl->cursor_theme, "left_ptr");
+    }
+    if (wl->cursor) {
+      wl->cursor_surface = wl_compositor_create_surface(wl->compositor);
+    }
+  }
+}
+
+void setupSurface(WL *wl){
+
+  // getting surfaces and mouse pointer
+  wl->surface = wl_compositor_create_surface(wl->compositor);
+  wl->pointer = wl_seat_get_pointer(wl->seat);
+  wl_pointer_add_listener(wl->pointer, &pointer_listener, wl);
+
+  // getting layer shell surface
+  wl->layer_surface = zwlr_layer_shell_v1_get_layer_surface(
+      wl->layer_shell, wl->surface, NULL, ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND,
+      "wallpaper");
+
+  // setting anchors for the surface
+  zwlr_layer_surface_v1_set_anchor(wl->layer_surface,
+                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP |
+                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM |
+                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT |
+                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT);
+
+  zwlr_layer_surface_v1_set_size(wl->layer_surface, 0, 0);
+  // trying to bypass all the exclusive zones
+  zwlr_layer_surface_v1_set_exclusive_zone(wl->layer_surface, -1);
+
+  // adding layer shell surface listener to layer shell surface
+  zwlr_layer_surface_v1_add_listener(wl->layer_surface, &layer_listener, wl);
+
+  // commiting the surface
+  wl_surface_commit(wl->surface);
+
+  while (!wl->configured) {
+    wl_display_dispatch(wl->display);
+  }
+
+  printf("\n\n%s %d\n", "[INFO]: Display Width = ", wl->width);
+  printf("%s %d\n\n", "[INFO]: Display Height = ", wl->height);
+}
+
+void setupEGL(WL *wl){
+  wl->egl_window = wl_egl_window_create(wl->surface, wl->width, wl->height);
+  if (!wl->egl_window) {
+      fprintf(stderr, "[ERR][EGL]: Failed to create egl window\n");
+      exit(1);
+  }
+  wl->egl_display = eglGetDisplay((EGLNativeDisplayType)wl->display);
+  if (!eglInitialize(wl->egl_display, NULL, NULL)) {
+      fprintf(stderr, "[ERR][EGL]: Init failed\n");
+      exit(1);
+  }
+
+  EGLint attributes[] =
+  {
+    EGL_SURFACE_TYPE,
+    EGL_WINDOW_BIT,
+    EGL_RENDERABLE_TYPE,
+    EGL_OPENGL_ES2_BIT,
+    EGL_NONE
+  };
+
+  EGLConfig config;
+  EGLint num_configs;
+  if (!eglChooseConfig(wl->egl_display, attributes, &config, 1, &num_configs) || num_configs < 1) {
+    fprintf(stderr,"\n[ERR][EGL]: Can't get egl config\n");
+  }
+
+  /* this will allocate gpu framebuffers,
+   * egl_surface is where the gpu will render
+  */
+  wl->egl_surface = 
+    eglCreateWindowSurface(wl->egl_display, config, (EGLNativeWindowType)wl->egl_window, NULL);
+
+  if (wl->egl_surface == EGL_NO_SURFACE) {
+      fprintf(stderr, "[ERR][EGL]: Failed to create surface\n");
+      exit(1);
+  }
+
+  eglBindAPI(EGL_OPENGL_ES_API);
+
+  EGLint ctxAttribs[] = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE};
+
+  wl->egl_context = eglCreateContext(wl->egl_display, config, EGL_NO_CONTEXT, ctxAttribs);
+  if (wl->egl_context == EGL_NO_CONTEXT) {
+      fprintf(stderr, "[ERR][EGL]: Failed to create context\n");
+      exit(1);
+  }
+  if (!eglMakeCurrent(wl->egl_display, wl->egl_surface, wl->egl_surface, wl->egl_context)) {
+    fprintf(stderr,"\n[ERR][EGL]: eglMakeCurrent failed\n");
+  }
+}
+
+int setupDaemonSocket(int daemon_sock){
+  struct sockaddr_un d_addr = {0};
+  d_addr.sun_family = AF_UNIX;
+  strncpy(d_addr.sun_path, SOCK_PATH, sizeof(d_addr.sun_path) - 1);
+  d_addr.sun_path[sizeof(d_addr.sun_path) - 1] = '\0';
+  unlink(SOCK_PATH);
+
+  if (bind(daemon_sock, (struct sockaddr*)&d_addr, sizeof(d_addr)) == -1) {
+    fprintf(stderr,"[ERR][SOCK]: Failed to bind socket!");
+    close(daemon_sock);
+    return 1;
+  }
+
+  printf("[INFO]: Socket created\n");
+  listen(daemon_sock, 5);
+  printf("[INFO]: Listening...\n\n");
+  int flags = fcntl(daemon_sock, F_GETFL, 0);
+  fcntl(daemon_sock, F_SETFL, flags | O_NONBLOCK);
+  return 0;
+}
+
+int setupOpenGL(WL *wl, GL *gl){
+
+  if ((gl->prog =
+      createProgram("/usr/share/wallrift/shaders/wallpaper.vert",
+                    "/usr/share/wallrift/shaders/wallpaper.frag")) == -1) {
+    
+    fprintf(stderr,"\n[ERR][GL]: Failed to create program.\n");
+    return 1;
+  }
+  gl->cursorLoc = glGetUniformLocation(gl->prog, "u_cursor");
+  gl->imgWLoc = glGetUniformLocation(gl->prog, "u_img_width");
+  gl->imgHLoc = glGetUniformLocation(gl->prog, "u_img_height");
+  gl->viewWLoc = glGetUniformLocation(gl->prog, "u_view_width");
+  gl->viewHLoc = glGetUniformLocation(gl->prog, "u_view_height");
+  
+  glUseProgram(gl->prog);
+  glGenBuffers(1, &gl->vbo);
+  glGenBuffers(1, &gl->ebo);
+
+  glBindBuffer(GL_ARRAY_BUFFER, gl->vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gl->ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  int resLoc = glGetUniformLocation(gl->prog, "resolution");
+  glUniform2f(resLoc, wl->width, wl->height);
+  int imgLoc = glGetUniformLocation(gl->prog, "imgSize");
+  glUniform2f(imgLoc, img_w, img_h);
+  return 0;
+}
+
 int main() {
 
   char wallpath[5000];
@@ -241,112 +430,14 @@ int main() {
     wallpath[0] = '\0';
   }
 
-
   WL wl = {0};
   GL gl = {0};
-  wl.run = 1;
-  wl.cursor_x = 0.05f; // initialize it at centre 
-  wl.display = wl_display_connect(NULL);
-  wl.registry = wl_display_get_registry(wl.display);
-  wl_registry_add_listener(wl.registry, &registry_listener, &wl);
-  wl_display_roundtrip(wl.display);
-
-  // cursor stuff
-  if (wl.shm) {
-    
-    /* supports X_CURSOR only cause hyprcursor uses
-     * different format for cursor and does not have API
-     * to work with.
-     */
-    char* theme = getenv("XCURSOR_THEME");
-    char* cursor_size = getenv("XCURSOR_SIZE");
-    
-    int size = cursor_size ? atoi(cursor_size) : 24;
-
-    wl.cursor_theme = wl_cursor_theme_load(theme,size, wl.shm);
-    if (wl.cursor_theme) {
-      wl.cursor = wl_cursor_theme_get_cursor(wl.cursor_theme, "default");
-    }
-    if (!wl.cursor) {
-      wl.cursor = wl_cursor_theme_get_cursor(wl.cursor_theme, "left_ptr");
-    }
-    if (wl.cursor) {
-      wl.cursor_surface = wl_compositor_create_surface(wl.compositor);
-    }
-  }
-
-  // getting surfaces and mouse pointer
-  wl.surface = wl_compositor_create_surface(wl.compositor);
-  wl.pointer = wl_seat_get_pointer(wl.seat);
-  wl_pointer_add_listener(wl.pointer, &pointer_listener, &wl);
-
-  // getting layer shell surface
-  wl.layer_surface = zwlr_layer_shell_v1_get_layer_surface(
-      wl.layer_shell, wl.surface, NULL, ZWLR_LAYER_SHELL_V1_LAYER_BACKGROUND,
-      "wallpaper");
-
-  // setting anchors for the surface
-  zwlr_layer_surface_v1_set_anchor(wl.layer_surface,
-                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_TOP |
-                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_BOTTOM |
-                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT |
-                                   ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT);
-
-  zwlr_layer_surface_v1_set_size(wl.layer_surface, 0, 0);
-  // trying to bypass all the exclusive zones
-  zwlr_layer_surface_v1_set_exclusive_zone(wl.layer_surface, -1);
-
-  // adding layer shell surface listener to layer shell surface
-  zwlr_layer_surface_v1_add_listener(wl.layer_surface, &layer_listener, &wl);
-
-  // commiting the surface
-  wl_surface_commit(wl.surface);
-
-  while (!wl.configured) {
-    wl_display_dispatch(wl.display);
-  }
-
-  printf("\n\n%s %d\n", "[INFO]: Display Width = ", wl.width);
-  printf("%s %d\n\n", "[INFO]: Display Height = ", wl.height);
-
-  // egl stuff
-  wl.egl_window = wl_egl_window_create(wl.surface, wl.width, wl.height); // encapsulate wl_surface in egl_surface
-  wl.egl_display = eglGetDisplay((EGLNativeDisplayType)wl.display); // connect egl to wayland
-  eglInitialize(wl.egl_display, NULL, NULL);
-
-  EGLint attributes[] = {
-                          EGL_SURFACE_TYPE,
-                          EGL_WINDOW_BIT,
-                          EGL_RENDERABLE_TYPE,
-                          EGL_OPENGL_ES2_BIT,
-                          EGL_NONE
-                        };
-
-  EGLConfig config;
-  EGLint num_configs;
-  if (!eglChooseConfig(wl.egl_display, attributes, &config, 1, &num_configs)) {
-    printf("\n[ERR][EGL]: Cant get egl config\n");
-  }
-
-  /* this will allocate gpu framebuffers,
-   * egl_surface is where the gpu will render
-  */
-  wl.egl_surface = eglCreateWindowSurface(
-      wl.egl_display, config, (EGLNativeWindowType)wl.egl_window, NULL);
-
-  eglBindAPI(EGL_OPENGL_ES_API);
-
-  EGLint ctxAttribs[] = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE};
-
-  wl.egl_context =
-      eglCreateContext(wl.egl_display, config, EGL_NO_CONTEXT, ctxAttribs);
-
-  if (!eglMakeCurrent(wl.egl_display, wl.egl_surface, wl.egl_surface, wl.egl_context)) {
-    printf("\n[ERR][EGL]: eglMakeCurrent failed\n");
-  }
+  setupWayland(&wl);
+  setupCursor(&wl);
+  setupSurface(&wl);
+  setupEGL(&wl);
   
-  // GLSL stuff
-  
+  // openGL stuff
   GLuint textureId = 0;
 
   // load the cached wallpaper
@@ -355,65 +446,26 @@ int main() {
      cache_wallpaper(wallpath);
   }
 
-  if ((gl.prog =
-      createProgram("/usr/share/wallrift/shaders/wallpaper.vert",
-                    "/usr/share/wallrift/shaders/wallpaper.frag")) == -1) {
-    
-    printf("\n[ERR][GL]: Failed to create program.\n");
+  if (setupOpenGL(&wl, &gl)) {
     return 1;
   }
-  
-
-  gl.cursorLoc = glGetUniformLocation(gl.prog, "u_cursor");
-  gl.imgWLoc = glGetUniformLocation(gl.prog, "u_img_width");
-  gl.imgHLoc = glGetUniformLocation(gl.prog, "u_img_height");
-  gl.viewWLoc = glGetUniformLocation(gl.prog, "u_view_width");
-  gl.viewHLoc = glGetUniformLocation(gl.prog, "u_view_height");
 
   // unix socket
   int daemon_sock = socket(AF_UNIX, SOCK_STREAM, 0);
-
-  struct sockaddr_un d_addr = {0};
-  d_addr.sun_family = AF_UNIX;
-  strncpy(d_addr.sun_path, SOCK_PATH, sizeof(d_addr.sun_path) - 1);
-  unlink(SOCK_PATH);
-
-  if (bind(daemon_sock, (struct sockaddr*)&d_addr, sizeof(d_addr)) == -1) {
-    perror("[ERR][SOCK]: Failed to bind socket!");
-    close(daemon_sock);
+  if (daemon_sock == -1) {
+    perror("Socket\n");
+    return 1;
+  }
+  
+  if (setupDaemonSocket(daemon_sock)) {
     return 1;
   }
 
-  printf("[INFO]: Socket created\n");
-  listen(daemon_sock, 5);
-  printf("[INFO]: Listening...\n\n");
-  int flags = fcntl(daemon_sock, F_GETFL, 0);
-  fcntl(daemon_sock, F_SETFL, flags | O_NONBLOCK);
-
-  glUseProgram(gl.prog);
-  glGenBuffers(1, &gl.vbo);
-  glGenBuffers(1, &gl.ebo);
-
-  glBindBuffer(GL_ARRAY_BUFFER, gl.vbo);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gl.ebo);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-
   int texloc = glGetUniformLocation(gl.prog, "tex");
-  int resLoc = glGetUniformLocation(gl.prog, "resolution");
-  glUniform2f(resLoc, wl.width, wl.height);
-  int imgLoc = glGetUniformLocation(gl.prog, "imgSize");
-  glUniform2f(imgLoc, img_w, img_h);
 
   // getting stuff ready for polling
   wl.wayland_fd = wl_display_get_fd(wl.display);
-
   struct pollfd fds[2];
-
   fds[0].fd = wl.wayland_fd;
   fds[0].events = POLLIN;
 
@@ -432,7 +484,8 @@ int main() {
       continue;
     }
     wl_display_flush(wl.display);
-    int ret = poll(fds, 2, 8);
+    int timeout = wl.run ? 0 : -1;
+    int ret = poll(fds, 2, timeout);
 
     if (ret > 0) {
       // wayland event happend
@@ -473,11 +526,14 @@ int main() {
 
             if (path && strcmp(path, wallpath) != 0) {
               snprintf(wallpath, sizeof(wallpath), "%s",path);
-              if (textureId != 0) {
-                glDeleteTextures(1, &textureId);
+              GLuint nexTex = loadImageIntoGPU(wallpath, &img_w, &img_h, textureId);
+              if (textureId != nexTex) {
+                if (textureId != 0) {
+                  glDeleteTextures(1, &textureId);
+                }
+                textureId = nexTex;
+                cache_wallpaper(wallpath);
               }
-              textureId = loadImageIntoGPU(wallpath, &img_w, &img_h, textureId);
-              cache_wallpaper(wallpath);
               gl_draw(&wl, &gl, texloc, textureId);
             }
           }
